84a43bc8e4d0072e93e18bbc0eb51b6dfd0bacda
Maggie <Maggie.Hoang.Nguyen@gmail.com>
2025-05-16 02:16:29 -0700

    Initial Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Here you can grab form data and process it
+    department = request.form.get('department')
+    courses = []
+    for i in range(1, 3):  # because we made 2 course boxes for now
+        course = request.form.get(f'course_{i}')
+        professor = request.form.get(f'professor_{i}')
+        grade = request.form.get(f'grade_{i}')
+        if course or professor or grade:
+            courses.append({
+                'course': course,
+                'professor': professor,
+                'grade': grade
+            })
+    # For now just print to console
+    print('Department:', department)
+    print('Courses:', courses)
+    return "Form submitted! Check your terminal."
+
+if __name__ == '__main__':
+    app.run(debug=True)
